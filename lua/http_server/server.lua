63a095113dc8730f3817589091ba66537536762d
Ludvik Kristoffersen <70239345+luddekn@users.noreply.github.com>
2024-12-11 23:54:23 +0100

    Update server.lua, updated logic

--- lua/http_server/server.lua
+++ lua/http_server/server.lua
@@ -1,4 +1,3 @@
-local server_job_id = nil
 function is_server_running()
     local result = vim.fn.systemlist("ps aux | grep '[p]ython3 -m http.server'")
     return #result > 0
@@ -10,29 +9,17 @@ function StartHttpServer()
         return
     end
 
-    local command = { "python3", "-m", "http.server", "8000" }
+    local command = {"python3", "-m", "http.server", "8000"}
 
-    server_job_id = vim.fn.jobstart(command, {
-        on_exit = function(_, code)
-            if code == 0 then
-                print("Python HTTP server stopped.")
-            else
-                print("Error: Server exited with code " .. code)
-            end
-        end,
-    })
+    vim.fn.jobstart(command)
 
-    if server_job_id > 0 then
-        print("Python HTTP server started on port 8000.")
-        vim.fn.system("xdg-open http://localhost:8000")
-    else
-        print("Failed to start the HTTP server.")
-    end
+    print("Python HTTP server started on port 8000.")
+    vim.fn.system("xdg-open http://localhost:8000")
 end
 
 function StopHttpServer()
     local result = vim.fn.systemlist("ps aux | grep '[p]ython3 -m http.server'")
-
+    
     if #result == 0 then
         print("No HTTP server is running.")
         return
@@ -41,14 +28,13 @@ function StopHttpServer()
     local pid = result[1]:match("%S+")
     vim.fn.system("kill " .. pid)
 
-    server_job_id = nil
     print("Python HTTP server stopped.")
 end
 
-vim.api.nvim_create_user_command("StartHTTPServer", function()
+vim.api.nvim_create_user_command("StartHTTPServer", function ()
     StartHttpServer()
 end, {})
 
-vim.api.nvim_create_user_command("StopHttpServer", function()
+vim.api.nvim_create_user_command("StopHttpServer", function ()
     StopHttpServer()
 end, {})
